9ca5273c5cdec96e69514274eea5d1ffd44ee4ae
Ben Dawson <17676646+bdawson1993@users.noreply.github.com>
2020-02-10 14:04:39 +0000

    latest changes

---
+++ Assets/Scripts/VRRot.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VRRot : MonoBehaviour
+{
+    public Vector3 target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        OVRPose pos = transform.ToTrackingSpacePose(Camera.current);
+        transform.position.Set(pos.position.x, pos.position.y, pos.position.z);
+        transform.rotation.Set(pos.orientation.x, pos.orientation.y, pos.orientation.z, pos.orientation.w);
+    }
+}
