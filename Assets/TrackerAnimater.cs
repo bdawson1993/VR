adde756d570dc2eeb3e7d78cf4f64fc1ec329f26
Ben Dawson <17676646+bdawson1993@users.noreply.github.com>
2020-02-18 19:29:44 +0000

    latest changes

---
+++ Assets/TrackerAnimater.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Animations;
+
+public class TrackerAnimater : MonoBehaviour
+{
+    public ParentConstraint parent;
+    float currentX;
+
+    float lastX;
+    float deltaX;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        parent = GetComponent<ParentConstraint>();
+        currentX = transform.position.x;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        deltaX = (transform.position.x - lastX);
+        lastX = transform.position.x;
+        Debug.Log(deltaX);
+    }
+}
