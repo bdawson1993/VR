c29496de1f72f856945d9f4f0b441d2f25878d19
Ben Dawson <17676646+bdawson1993@users.noreply.github.com>
2020-02-27 22:27:01 +0000

    egg smash behavior and scene changes

---
+++ Assets/EggController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EggController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject inside;
+    public GameObject brokenShell;
+
+    Rigidbody outerShellRB;
+
+    void Start()
+    {
+        outerShellRB = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log(outerShellRB.velocity.magnitude);
+
+        if(outerShellRB.velocity.magnitude >= 1)
+        {
+            inside.SetActive(true);
+            inside.transform.position = transform.position;
+
+
+            brokenShell.SetActive(true);
+            brokenShell.transform.position = transform.position;
+
+
+            gameObject.SetActive(false);
+        }
+    }
+}
