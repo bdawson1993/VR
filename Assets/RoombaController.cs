c99742faa1fb170119ceb1011d2d0133d7ab925a
bdawson1993 <17676646+bdawson1993@users.noreply.github.com>
2020-03-04 14:34:24 +0000

    added roomba

---
+++ Assets/RoombaController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoombaController : MonoBehaviour
+{
+    Vector3 direc = Vector3.forward;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        GetComponent<Rigidbody>().AddForce(direc * 8.0f);
+     
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        direc = -direc;
+    }
+}
